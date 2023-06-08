cc1d010da36c1a63f934e0e37e68f0d357e9243f
rgnldo <58551674+rgnldo@users.noreply.github.com>
2023-06-08 11:58:46 -0300

    Update instal_zsh.sh

--- instal_zsh.sh
+++ instal_zsh.sh
@@ -18,27 +18,17 @@ if ! command -v zsh >/dev/null 2>&1; then
     exit 1
 fi
 
-# URL do arquivo zip
-url="https://github.com/rgnldo/knot-resolver-suricata/raw/master/_zsh.zip"
+# Baixa o arquivo ZIP
+curl -L -O https://github.com/rgnldo/knot-resolver-suricata/raw/master/_zsh.zip
 
-# Pasta de destino para descompactar o arquivo
-destination_folder="$HOME"
+# Descompacta o arquivo ZIP recursivamente no diretório do usuário
+unzip -o -q "$HOME" _zsh.zip
 
-# Nome do arquivo zip
-zip_file_name="_zsh.zip"
+# Define o shell padrão do usuário como zsh
+chsh -s /usr/bin/zsh "$USER"
 
-# Caminho completo do arquivo zip
-zip_file_path="$destination_folder/$zip_file_name"
+# Remove o arquivo ZIP
+rm _zsh.zip
 
-# Baixar o arquivo zip usando o wget e descompactar no diretório de destino
-wget "$url" -O "$zip_file_path" && unzip -q "$zip_file_path" -d "$destination_folder"
+echo "Instalação do Zsh concluída com sucesso!"
 
-# Verificar se o download e a descompactação foram bem-sucedidos
-if [ $? -eq 0 ]; then
-    echo "O arquivo zip foi baixado e descompactado com sucesso na pasta $destination_folder."
-else
-    echo "O download ou descompactação do arquivo zip falhou. Verifique a URL ou sua conexão de internet."
-    exit 1
-fi
-
-echo "O download e descompactação do arquivo zip foram concluídos com sucesso."
