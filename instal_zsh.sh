8e4686ab4d079ff0ebc2c2d25c6a2e4d887634f3
rgnldo <58551674+rgnldo@users.noreply.github.com>
2023-06-08 11:45:38 -0300

    Update instal_zsh.sh

--- instal_zsh.sh
+++ instal_zsh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Verificar se o comando 'curl' está disponível
-if ! command -v curl >/dev/null 2>&1; then
-    echo "O comando 'curl' não está instalado. Por favor, instale-o para continuar."
+# Verificar se o comando 'wget' está disponível
+if ! command -v wget >/dev/null 2>&1; then
+    echo "O comando 'wget' não está instalado. Por favor, instale-o para continuar."
     exit 1
 fi
 
@@ -30,32 +30,14 @@ zip_file_name="_zsh.zip"
 # Caminho completo do arquivo zip
 zip_file_path="$destination_folder/$zip_file_name"
 
-# Baixar o arquivo zip usando o curl
-curl -o "$zip_file_path" "$url"
+# Baixar o arquivo zip usando o wget e descompactar no diretório de destino
+wget "$url" -O "$zip_file_path" && unzip -q "$zip_file_path" -d "$destination_folder"
 
-# Verificar se o download foi bem-sucedido
+# Verificar se o download e a descompactação foram bem-sucedidos
 if [ $? -eq 0 ]; then
-    echo "O arquivo zip foi baixado com sucesso."
+    echo "O arquivo zip foi baixado e descompactado com sucesso na pasta $destination_folder."
 else
-    echo "O download do arquivo zip falhou. Verifique a URL ou sua conexão de internet."
-    exit 1
-fi
-
-# Verificar se o arquivo zip existe
-if [ ! -f "$zip_file_path" ]; then
-    echo "O arquivo zip não foi encontrado em $zip_file_path."
-    exit 1
-fi
-
-# Descompactar o arquivo zip na pasta de destino
-cd "$destination_folder"
-unzip -q "$zip_file_name"
-
-# Verificar se a descompactação foi bem-sucedida
-if [ $? -eq 0 ]; then
-    echo "O arquivo zip foi descompactado com sucesso na pasta $destination_folder."
-else
-    echo "A descompactação do arquivo zip falhou. Verifique se o arquivo zip é válido."
+    echo "O download ou descompactação do arquivo zip falhou. Verifique a URL ou sua conexão de internet."
     exit 1
 fi
 
