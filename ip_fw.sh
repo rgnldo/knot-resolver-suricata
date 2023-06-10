08b4934c5e16c7ca99dec37809732411fe23b88c
rgnldo <58551674+rgnldo@users.noreply.github.com>
2023-06-10 11:33:01 -0300

    Update ip_fw.sh

--- ip_fw.sh
+++ ip_fw.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# Limpar todas as regras e chains nas tabelas filter, nat e mangle
+echo "Limpar todas as regras e chains nas tabelas filter, nat e mangle"
 iptables -F
 iptables -t nat -F
 iptables -t mangle -F
 
-# Excluir todas as chains personalizadas
+echo "Excluir todas as chains personalizadas"
 iptables -X
 iptables -t nat -X
 iptables -t mangle -X
 
+echo "Removendo arquivos existentes"
 rm /etc/iptables/simple_firewall.rules
 rm /etc/iptables/ip6_simple_firewall.rules
 
-# Definir a política padrão como ACCEPT para todas as chains
+echo "Definir a política padrão como ACCEPT para todas as chains"
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
@@ -23,23 +24,23 @@ iptables -t nat -P POSTROUTING ACCEPT
 iptables -t mangle -P PREROUTING ACCEPT
 iptables -t mangle -P OUTPUT ACCEPT
 
-# Bloqueando vírus
+echo "Bloqueando vírus"
 iptables -N VIRUSPROT
 iptables -A VIRUSPROT -m limit --limit 3/minute --limit-burst 10 -j LOG --log-prefix "virusprot: "
 iptables -A VIRUSPROT -m conntrack --ctstate NEW -m recent --set --name DEFAULT --mask 255.255.255.255 --rsource 
 iptables -A VIRUSPROT -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 --name DEFAULT --mask 255.255.255.255 --rsource -j DROP
 iptables -A VIRUSPROT -j DROP
 
-# SSH lockout
+echo "SSH lockout"
 iptables -N SSHLOCKOUT
 iptables -A SSHLOCKOUT -m recent --name sshbf --set -j DROP
 iptables -A INPUT -p tcp --dport ssh -m recent --name sshbf --rcheck --seconds 300 --hitcount 4 -j SSHLOCKOUT
 
-# Bloqueando todos os portos de destino 0
+echo "Bloqueando todos os portos de destino 0"
 iptables -A INPUT -p tcp --destination-port 0 -j DROP
 iptables -A INPUT -p udp --destination-port 0 -j DROP
 
-# Regra padrão DROP e violações de estado
+echo "Regra padrão DROP e violações de estado"
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
@@ -48,7 +49,7 @@ iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state INVALID -j DROP
 
-# don't log noisy services by default
+echo " Don't log noisy services by default"
 iptables -A INPUT -p udp --dport 137 -j DROP
 iptables -A INPUT -p udp --dport 138 -j DROP
 iptables -A INPUT -p tcp --dport 139 -j DROP
@@ -56,59 +57,59 @@ iptables -A INPUT -p tcp --dport 445 -j DROP
 iptables -A INPUT -p udp --dport 67 -j DROP
 iptables -A INPUT -p udp --dport 68 -j DROP
 
-# Permitindo ICMPv6 e CARP
+echo "Permitindo ICMPv6 e CARP"
 ip6tables -A INPUT -p icmpv6 -j ACCEPT
 ip6tables -A INPUT -m state --state INVALID -j DROP
 ip6tables -A INPUT -p vrrp -j ACCEPT
 
-# Permitir o serviço NetBIOS (UDP)
+echo "Permitir o serviço NetBIOS (UDP)"
 iptables -A INPUT -p udp --dport 137 -j ACCEPT
 iptables -A INPUT -p udp --dport 138 -j ACCEPT
 
-# Permitir o compartilhamento de arquivos e impressoras do Windows (TCP)
+echo "Permitir o compartilhamento de arquivos e impressoras do Windows (TCP)"
 iptables -A INPUT -p tcp --dport 139 -j ACCEPT
 iptables -A INPUT -p tcp --dport 445 -j ACCEPT
 
-# Permitir o serviço DHCP (UDP)
+echo "Permitir o serviço DHCP (UDP)"
 iptables -A INPUT -p udp --dport 67 -j ACCEPT
 iptables -A INPUT -p udp --dport 68 -j ACCEPT
 
-# Permitir pacotes relacionados ou estabelecidos
+echo "Permitir pacotes relacionados ou estabelecidos"
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Permitir pacotes destinados ao endereço multicast mDNS
+echo "Permitir pacotes destinados ao endereço multicast mDNS"
 iptables -A INPUT -p udp -d 224.0.0.251 --dport 5353 -j ACCEPT
 
-# Permitir pacotes destinados ao endereço multicast UPnP
+echo "Permitir pacotes destinados ao endereço multicast UPnP"
 iptables -A INPUT -p udp -d 239.255.255.250 --dport 1900 -j ACCEPT
 
-# Permitir pacotes destinados ao endereço local
+echo "Permitir pacotes destinados ao endereço local"
 iptables -A INPUT -i lo -j ACCEPT
 
-# Proteção contra SYN flood
+echo "Proteção contra SYN flood"
 iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 10 --connlimit-mask 32 -j DROP
 
-# Proteção contra ataques de inundação de ICMP
+echo "Proteção contra ataques de inundação de ICMP"
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 10 -j ACCEPT
 
-# Proteção contra ataques de negação de serviço
+echo "Proteção contra ataques de negação de serviço"
 iptables -A INPUT -p tcp --dport 80 -m limit --limit 25/s --limit-burst 100 -j ACCEPT
 
-# Limitar a taxa de abertura de conexões por segundo
+echo "Limitar a taxa de abertura de conexões por segundo"
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT
 
-# Restringir a execução de páginas de memória
+echo "Restringir a execução de páginas de memória"
 iptables -A INPUT -m addrtype --dst-type LOCAL -m limit --limit 1/s --limit-burst 10 -j ACCEPT
 
-# Proteção contra estouro de buffer
+echo "Proteção contra estouro de buffer"
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -m limit --limit 1/h -j ACCEPT
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -m limit --limit 1/h -j ACCEPT
 
-# Ocultar kernel pointers
+echo "Ocultar kernel pointers"
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
 
-# Permitir respostas ACK/RTS para acelerar a comunicação
+echo "Permitir respostas ACK/RTS para acelerar a comunicação"
 iptables -A INPUT -p tcp --tcp-flags ACK,FIN FIN -m limit --limit 1/s --limit-burst 10 -j ACCEPT
 iptables -A INPUT -p tcp --tcp-flags ACK,PSH PSH -m limit --limit 1/s --limit-burst 10 -j ACCEPT
 iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -m limit --limit 1/s --limit-burst 10 -j ACCEPT
@@ -116,6 +117,7 @@ iptables -A INPUT -p tcp --tcp-flags ACK,URG URG -m limit --limit 1/s --limit-bu
 # Salvar regras
 iptables-save > /etc/iptables/simple_firewall.rules
 ip6tables-save > /etc/iptables/ip6_simple_firewall.rules
-iptables-restore < /etc/iptables/simple_firewall.rules
-ip6tables-restore < /etc/iptables/ip6_simple_firewall.rules
-# Regras de segurança adicionais
+
+echo "Iniciando serviços IPTABLES"
+sudo systemctl enable iptables.service
+sudo systemctl start iptables.service
