5d1a14d65a9250383d8e9bf3b3d512419c45af3f
rgnldo <58551674+rgnldo@users.noreply.github.com>
2023-10-08 10:46:54 -0300

    Create Aut_upd_ubuntu.sh

---
+++ Aut_upd_ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Verifica se o pacote unattended-upgrades está instalado
+if ! dpkg -l | grep -q unattended-upgrades; then
+    echo "O pacote unattended-upgrades não está instalado. Instalando..."
+    sudo apt update
+    sudo apt install unattended-upgrades -y
+fi
+
+# URL do arquivo 50unattended-upgrades no GitHub
+URL="https://raw.githubusercontent.com/rgnldo/knot-resolver-suricata/master/50unattended-upgrades"
+
+# Pasta de destino
+DEST_FOLDER="/etc/apt/apt.conf.d/"
+
+# Nome do arquivo de destino
+DEST_FILE="50unattended-upgrades"
+
+# Baixa o arquivo do GitHub e o coloca na pasta de destino
+sudo wget "$URL" -O "${DEST_FOLDER}${DEST_FILE}"
+
+# Verifica se o arquivo foi baixado com sucesso
+if [ $? -eq 0 ]; then
+    echo "Arquivo baixado com sucesso."
+
+    # Ativa e inicia o serviço unattended-upgrades
+    sudo systemctl enable unattended-upgrades
+    sudo systemctl start unattended-upgrades
+
+    echo "Serviço unattended-upgrades ativado e iniciado."
+else
+    echo "Falha ao baixar o arquivo."
+fi
