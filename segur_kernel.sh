56274a04473eb85fb7595f7d633d06e93645879e
rgnldo <58551674+rgnldo@users.noreply.github.com>
2023-11-21 09:24:01 -0300

    Update perform_linux.sh

--- segur_kernel.sh
+++ segur_kernel.sh
@@ -1,42 +1,90 @@
 #!/bin/bash
 
-echo "Adicionando opções de segurança no kernel"
+# Arquivo de configuração do sysctl
+sysctl_conf_file="/etc/sysctl.d/99-sysctl.conf"
 
-# Ignore ICMP broadcast requests
-echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.d/99-sysctl.conf
+# Função para adicionar uma regra ao arquivo sysctl_conf_file
+add_sysctl_rule() {
+    local rule=$1
+    grep -qF "$rule" "$sysctl_conf_file" || echo "$rule" >> "$sysctl_conf_file"
+}
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.d/99-sysctl.conf
+# Verificar se o arquivo de configuração já existe
+if [[ -e "$sysctl_conf_file" ]]; then
+    echo "O arquivo $sysctl_conf_file já existe. Verificando configurações existentes..."
+else
+    echo "Criando o arquivo $sysctl_conf_file..."
+    touch "$sysctl_conf_file"
+fi
 
-echo "net.core.rmem_default=1048576" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.core.rmem_max=16777216" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.core.wmem_default=1048576" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.core.wmem_max=16777216" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.core.optmem_max=65536" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_rmem=4096 1048576 2097152" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_wmem=4096 65536 16777216" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.udp_rmem_min=8192" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.udp_wmem_min=8192" >> /etc/sysctl.d/99-sysctl.conf 
+# Regras de segurança para o kernel
 
-# Block SYN attacks
-echo "net.ipv4.tcp_syncookies=1" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_max_syn_backlog=2048" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_synack_retries=2" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.tcp_syn_retries=5" >> /etc/sysctl.d/99-sysctl.conf
+# Proteção contra SYN flood
+add_sysctl_rule "# Proteção contra SYN flood"
+add_sysctl_rule "net.ipv4.tcp_syncookies = 1"
 
-# Log Martians
-echo "net.ipv4.conf.all.log_martians=1" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.icmp_ignore_bogus_error_responses=1" >> /etc/sysctl.d/99-sysctl.conf
+# Restringir a execução de páginas de memória
+add_sysctl_rule "# Restringir a execução de páginas de memória"
+add_sysctl_rule "vm.mmap_min_addr = 65536"
 
-# IP Spoofing protection
-echo "net.ipv4.conf.all.rp_filter=1" >> /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv4.conf.default.rp_filter=1" >> /etc/sysctl.d/99-sysctl.conf
+# Proteção contra estouro de buffer
+add_sysctl_rule "# Proteção contra estouro de buffer"
+add_sysctl_rule "kernel.randomize_va_space = 2"
 
-# Ignore ICMP broadcast requests
-echo "net.ipv4.icmp_echo_ignore_broadcasts=1" >> /etc/sysctl.d/99-sysctl.conf
+# Proteção contra ataques de inundação de ICMP
+add_sysctl_rule "# Proteção contra ataques de inundação de ICMP"
+add_sysctl_rule "net.ipv4.icmp_echo_ignore_broadcasts =1"
+add_sysctl_rule "net.ipv4.icmp_ignore_bogus_error_responses=1"
 
-# Hide kernel pointers
-echo "kernel.kptr_restrict=2" >> /etc/sysctl.d/99-sysctl.conf 
+# Proteção contra ataques de negação de serviço
+add_sysctl_rule "# Proteção contra ataques de negação de serviço"
+add_sysctl_rule "net.ipv4.tcp_max_syn_backlog=2048"
+add_sysctl_rule "net.ipv4.tcp_synack_retries=2"
+add_sysctl_rule "net.ipv4.tcp_syn_retries=5"
 
-# Enable panic on OOM
-echo "vm.panic_on_oom=1" >> /etc/sysctl.d/99-sysctl.conf
+# Limitar a taxa de abertura de conexões por segundo
+add_sysctl_rule "# Limitar a taxa de abertura de conexões por segundo"
+add_sysctl_rule "net.ipv4.tcp_max_syn_backlog=2048"
+add_sysctl_rule "net.ipv4.tcp_synack_retries=2"
+add_sysctl_rule "net.ipv4.tcp_syn_retries=5"
+
+# Acelerar respostas ACK RTS
+add_sysctl_rule "# Acelerar respostas ACK RTS"
+add_sysctl_rule "net.ipv4.tcp_rfc1337=1"
+add_sysctl_rule "net.ipv4.tcp_sack=1"
+add_sysctl_rule "net.ipv4.tcp_timestamps=1"
+add_sysctl_rule "net.ipv4.tcp_window_scaling=1"
+
+# Outras configurações
+add_sysctl_rule "# Outras configurações"
+add_sysctl_rule "net.core.rmem_default=262144"
+add_sysctl_rule "net.core.rmem_max=524288"
+add_sysctl_rule "net.core.wmem_default=262144"
+add_sysctl_rule "net.core.wmem_max=524288"
+add_sysctl_rule "net.ipv4.tcp_fastopen=3"
+add_sysctl_rule "net.ipv4.tcp_retries1=3"
+add_sysctl_rule "net.ipv4.tcp_retries2=5"
+
+add_sysctl_rule "# Incremento nas conexões TCP"
+add_sysctl_rule "net.core.default_qdisc=fq"
+add_sysctl_rule "net.ipv4.tcp_congestion_control=bbr"
+
+# Remover linhas vazias duplicadas do arquivo sysctl_conf_file
+sed -i '/^$/d' "$sysctl_conf_file"
+
+echo "As configurações foram adicionadas ao arquivo $sysctl_conf_file."
+
+# Reiniciar o sistema para garantir que as alterações sejam aplicadas completamente
+read -p "Deseja reiniciar o sistema agora para aplicar completamente as alterações? (s/n): " choice
+
+case "$choice" in
+    s|S)
+        echo "Reiniciando o sistema..."
+        systemctl reboot
+        ;;
+    *)
+        echo "Reinicie o sistema posteriormente para aplicar completamente as alterações."
+        ;;
+esac
+
+exit 0
