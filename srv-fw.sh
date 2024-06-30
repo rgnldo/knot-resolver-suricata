bba2e58593fddc5150c6cae317e3062cc13e1cc1
rgnldo <58551674+rgnldo@users.noreply.github.com>
2024-06-30 18:55:35 -0300

    Update srv-fw.sh

--- srv-fw.sh
+++ srv-fw.sh
@@ -10,8 +10,8 @@ iptables -t nat -X
 iptables -t mangle -X
 
 echo "Removendo arquivos existentes"
-rm /etc/iptables/simple_firewall.rules
-rm /etc/iptables/ip6_simple_firewall.rules
+rm /etc/iptables/test_firewall.rules
+rm /etc/iptables/test6_firewall.rules
 
 # Obtendo nome da interface principal
 nic=$(ip route get 1.1.1.1 | awk '{print $5}')
@@ -93,49 +93,106 @@ if [ -n "$wireguard_iface" ]; then
   iptables -A FORWARD -i $wireguard_iface -o $nic -j ACCEPT
 fi
 
-# Permitindo loopback
-iptables -A INPUT -i lo -j ACCEPT
-
-# Permitindo tráfego já estabelecido e relacionado
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# Regra padrão DROP e violações de estado
+echo "Regra padrão DROP e violações de estado"
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m state --state INVALID -j DROP
 
 # Limitar número de conexões TCP por IP
+echo "Limitando número de conexões TCP por IP"
 iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 20 --connlimit-mask 32 -j DROP
 
 # Limitar pacotes ICMP Echo-Request
+echo "Limitando pacotes ICMP Echo-Request"
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 5/second --limit-burst 10 -j ACCEPT
 
 # Limitar taxa de novas conexões TCP na porta 80
+echo "Limitando taxa de novas conexões TCP na porta 80"
 iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m recent --set
 iptables -A INPUT -p tcp --dport 80 -m state --state NEW -m recent --update --seconds 60 --hitcount 25 -j DROP
 
-# Permitindo loopback
-iptables -A INPUT -i lo -j ACCEPT
-
-# Permitindo tráfego já estabelecido e relacionado
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
 # Proteção contra estouro de buffer
 echo "Proteção contra estouro de buffer"
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -m limit --limit 1/h -j ACCEPT
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -m limit --limit 1/h -j ACCEPT
-
-# Proteção contra varreduras de porta
-echo "Proteção contra vazamentos"
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -m limit --limit 1/hour -j ACCEPT
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -m limit --limit 1/hour -j ACCEPT
 
 # Bloqueando tráfego destinado à porta 0
 echo "Bloqueando tráfego destinado à porta 0"
 iptables -A INPUT -p tcp --destination-port 0 -j DROP
 iptables -A INPUT -p udp --destination-port 0 -j DROP
 
-# Regra padrão DROP e violações de estado
-echo "Regra padrão DROP e violações de estado"
-iptables -A INPUT -p icmp -j ACCEPT
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
+##############################
+### ATAQUES
+##############################
+echo "Proteções contra ataques"
+# Todas as sessões TCP devem começar com SYN
+iptables -A INPUT -p tcp ! --syn -m state --state NEW -s 0.0.0.0/0 -j DROP
+
+# Proteção contra SYN Flood
+echo "Proteção contra SYN Flood"
+iptables -N syn_flood
+iptables -A INPUT -p tcp --syn -j syn_flood
+iptables -A syn_flood -m limit --limit 1/s --limit-burst 3 -j RETURN
+iptables -A syn_flood -j DROP
+
+# ICMP fragmentado - sinal de ataque DoS
+echo "Proteção contra ataques DoS ICMP fragmentados"
+iptables -A INPUT --fragment -p ICMP -j DROP
+
+# Limitando as requisições de ping ICMP recebidas
+echo "Limitando requisições de ping ICMP"
+iptables -A INPUT -p icmp -m limit --limit 1/s --limit-burst 1 -j ACCEPT
+iptables -A INPUT -p icmp -j DROP
+iptables -A OUTPUT -p icmp -j ACCEPT
+
+# Forçar verificação de pacotes fragmentados
+echo "Forçando verificação de pacotes fragmentados"
+iptables -A INPUT -f -j DROP
+
+# Pacotes XMAS malformados recebidos
+echo "Proteção contra pacotes XMAS malformados"
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+
+# Dropar todos os pacotes NULL
+echo "Proteção contra pacotes NULL"
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+# Pacotes inválidos e suspeitos
+echo "Proteção contra pacotes inválidos e suspeitos"
 iptables -A INPUT -m state --state INVALID -j DROP
+
+# Proteções contra varreduras furtivas
+echo "Proteção contra varreduras furtivas"
+# Varredura furtiva 1
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j LOG --log-prefix "FWLOG: Stealth scan (1): "
+iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
+
+# Varredura furtiva 2
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j LOG --log-prefix "FWLOG: Stealth scan (2): "
+iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+
+# Varredura furtiva 3
+iptables -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j LOG --log-prefix "FWLOG: Stealth scan (3): "
+iptables -A INPUT -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+
+# Varredura furtiva 4
+iptables -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j LOG --log-prefix "FWLOG: Stealth scan (4): "
+iptables -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+
+# Varredura furtiva 5
+iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j LOG --log-prefix "FWLOG: Stealth scan (5): "
+iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+
+# Varredura furtiva 6
+iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j LOG --log-prefix "FWLOG: Stealth scan (6): "
+iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+
+# Varredura de portas
+echo "Proteção contra varredura de portas"
+iptables -N port-scan
+iptables -A port-scan -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j RETURN
+iptables -A port-scan -j DROP
 
 # Permitindo ICMPv6 e CARP
 echo "Permitindo ICMPv6 e CARP"
@@ -143,39 +200,28 @@ ip6tables -A INPUT -p icmpv6 -j ACCEPT
 ip6tables -A INPUT -m state --state INVALID -j DROP
 ip6tables -A INPUT -p vrrp -j ACCEPT
 
-# Permitindo pacotes destinados ao endereço local
-echo "Permitindo pacotes destinados ao endereço local"
+# Permitindo ssh, www, https e letsencrypt
+echo "Permitindo ssh, www, https e letsencrypt"
+iptables -A OUTPUT -p tcp -m multiport --dports 22,80,443,54321 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sports 22,80,443,54321 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 22,80,443,54321 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 22,80,443,54321 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 995,3128,992,5555,8080 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sports 995,3128,992,5555,8080 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 995,3128,992,5555,8080 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 995,3128,992,5555,8080 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
-
-# Permitindo HTTPS e HTTP (TCP)
-echo "Permitindo HTTPS e HTTP (TCP)"
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-echo "Permitindo UDP 443 (UDP)"
-iptables -A INPUT -p udp --dport 443 -j ACCEPT
-
-# Permitindo DNS (TCP e UDP)
-echo "Permitindo DNS (TCP e UDP)"
-iptables -A INPUT -p tcp --dport 53 -j ACCEPT
-iptables -A INPUT -p udp --dport 53 -j ACCEPT
-
-# Permitindo SSH
-echo "Permitindo SSH"
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
-# Logging de pacotes suspeitos
-iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -p udp -m multiport --dports 53,67,68 -j ACCEPT
+iptables -A INPUT -p udp -m multiport --sports 53,67,68 -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 53,67,68 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --sports 53,67,68 -j ACCEPT
 
 # Configurando marcação para tráfego aceito
 echo "Configurando marcação para tráfego aceito"
 iptables -t mangle -A OUTPUT -p tcp --dport 53 -j MARK --set-mark 1
 iptables -t mangle -A OUTPUT -p udp --dport 53 -j MARK --set-mark 1
 iptables -t mangle -A OUTPUT -p tcp --dport 443 -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -p udp --dport 443 -j MARK --set-mark 1
-iptables -t mangle -A OUTPUT -p tcp --dport 25 -j MARK --set-mark 3
-iptables -t mangle -A OUTPUT -p tcp --dport 110 -j MARK --set-mark 4
-iptables -t mangle -A OUTPUT -p tcp --dport 143 -j MARK --set-mark 5
 
 # Configurar QoS com tc
 echo "Configurando QoS com tc"
@@ -197,8 +243,8 @@ tc filter add dev $nic parent 1:0 protocol ip handle 2 fw flowid 1:20
 echo "QoS configurado com sucesso"
 
 # Salvar regras
-iptables-save > /etc/iptables/simple_firewall.rules
-ip6tables-save > /etc/iptables/ip6_simple_firewall.rules
+iptables-save > /etc/iptables/test_firewall.rules
+ip6tables-save > /etc/iptables/test6_firewall.rules
 
 echo "Gerando a execuç."
 fw_script_file="/usr/local/bin/fw.sh"
